3011465806e97ca19692fd1342ae5ebc8fc2d0bd
Denis Mwanzia <denismwanzia56@gmail.com>
2025-05-05 22:08:44 +0300

    Modified Task model to follow snake casing.

--- taskManager/mainApp/models.py
+++ taskManager/mainApp/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# Model: Task
 class Task(models.Model):
-    taskID = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200, blank=False, null=False)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    due_date = models.DateField(blank=False)
-    completed = models.BooleanField(default=False)
-    
+    due_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)  # False = Pending, True = Done
+
     def __str__(self):
         return self.title
+
     
 # 
