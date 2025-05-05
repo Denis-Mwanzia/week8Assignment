2204e27f10741d4d059e85bd77d383778f76637b
Denis Mwanzia <denismwanzia56@gmail.com>
2025-05-05 16:23:29 +0300

    Added SQL script to create department table.

--- clinicBooking/clinicBooking.sql
+++ clinicBooking/clinicBooking.sql
@@ -4,3 +4,8 @@ CREATE DATABASE clinicBookingDb;
 -- Use Database
 USE clinicBookingDb;
 
+-- Table: department
+CREATE TABLE department (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
