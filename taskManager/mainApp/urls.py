deb4a0f1928eaf190534e22fd56ac9c981f803e1
Denis Mwanzia <denismwanzia56@gmail.com>
2025-05-06 22:28:18 +0300

    Removed the unnecessary comment

--- taskManager/mainApp/urls.py
+++ taskManager/mainApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import TaskListCreateView, TaskDetailView, api_root
 
 urlpatterns = [
-    path('', api_root, name='api-root'),  # now /api/ works
+    path('', api_root, name='api-root'),  
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
 ]
