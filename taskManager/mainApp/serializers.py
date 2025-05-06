4f71beef7730d6d81e921fbeb9866d5862c3f704
Denis Mwanzia <denismwanzia56@gmail.com>
2025-05-06 06:44:52 +0300

    updated serializer to include user assigned the task.

--- taskManager/mainApp/serializers.py
+++ taskManager/mainApp/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import *
+from django.contrib.auth.models import User
+from .models import Task
 
-class taskSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'due_date', 'created_at', 'updated_at', 'status')
+        fields = '__all__'
+        read_only_fields = ['user', 'created_at', 'updated_at']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
