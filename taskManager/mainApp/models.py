f7abf576f04e2653e0cfe74719ecbeb23f123fd7
Denis Mwanzia <denismwanzia56@gmail.com>
2025-05-06 22:26:20 +0300

    Added status choices for task model

--- taskManager/mainApp/models.py
+++ taskManager/mainApp/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('WORKING', 'Working on It'),
+        ('DONE', 'Done'),
+    ]
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     due_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='PENDING')
 
     def __str__(self):
         return self.title
